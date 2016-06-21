d6d526f423ff8c3b3fef2c0d44ea53784528a683
andrewfok007 <andrewfok007@yahoo.co.uk>
2016-06-21 21:28:36 +0800

    Added controller and model classes to library

---
+++ library/controller.class.php
@@ -0,0 +1,33 @@
+<?php
+
+// Used for all communication between the controller,
+// the model and the view (template class).
+// It creates an object for the model class and an object for template class.
+// The object for model class has the same name as the model itself,
+// so that we can call it something like $this->Item->selectAll();
+// from our controller.
+
+
+class Controller {
+	protected $_model;
+	protected $_controller;
+	protected $_action;
+	protected $_template;
+
+	function __construct($model, $controller, $action){
+		$this->_controller = $controller;
+		$this->_action = $action;
+		$this->_model = $model;
+
+		$this->$model = &amp; new $model;
+		$this->_template = &amp; new Template($controller, $action);
+	}
+
+	function set($name, $value){
+		$this->_template->set($name, $value);
+	}
+
+	function __destruct(){
+		$this->_template->render();
+	}
+}
