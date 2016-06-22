8904d65df9fead22f55a698d48cfe20fdfd68baa
andrewfok007 <andrewfok007@yahoo.co.uk>
2016-06-23 07:28:49 +0800

    Template class for binding headers, action view and footer, sqlquery syntax fixes

--- library/sqlquery.class.php
+++ library/sqlquery.class.php
@@ -1,14 +1,17 @@
 <?php
 
+
 class SQLQuery {
     protected $_dbHandle;
-    proected $_result;
+    protected $_result;
 
-    function connect($address, $account, $pwd, $name){
-        $this->_dbHandle = @mysql_connect($address, $account, $pwd);
+    /** Connects to database **/
 
-        if($this->_dbHandle != 0){
-            if(mysql_select_db($name, $this->_dbHandle)){
+    function connect($address, $account, $pwd, $name) {
+        $this->_dbHandle = mysql_connect($address, $account, $pwd);
+        
+        if ($this->_dbHandle != 0) {
+            if (mysql_select_db($name, $this->_dbHandle)) {
                 return 1;
             } else {
                 return 0;
@@ -18,73 +21,76 @@ class SQLQuery {
         }
     }
 
+    /** Disconnects from database **/
+
     function disconnect() {
-        if(@mysql_close($this->_dbHandle) != 0){
+        if (mysql_close($this->_dbHandle) != 0) {
             return 1;
-        } else {
+        }  else {
             return 0;
         }
     }
-
+    
     function selectAll() {
         $query = 'select * from `'.$this->_table.'`';
         return $this->query($query);
     }
-
+    
     function select($id) {
         $query = 'select * from `'.$this->_table.'` where `id` = \''.mysql_real_escape_string($id).'\'';
-        return $this->query($query, 1);
+        return $this->query($query, 1);    
     }
 
+    
+    /** Custom SQL Query **/
+
     function query($query, $singleResult = 0) {
- 
-        $this->_result = mysql_query($query, $this->_dbHandle);
- 
+
+        $this->_result = mysql_query($query, $this->_dbHandle) or trigger_error(mysql_error()." in ".$sql);
+
         if (preg_match("/select/i", $query)) {
             $result = array();
             $table = array();
             $field = array();
             $tempResults = array();
             $numOfFields = mysql_num_fields($this->_result);
-
             for ($i = 0; $i < $numOfFields; ++$i) {
                 array_push($table, mysql_field_table($this->_result, $i));
                 array_push($field, mysql_field_name($this->_result, $i));
             }
-             
+        
             while ($row = mysql_fetch_row($this->_result)) {
-                for ($i = 0; $i < $numOfFields; ++$i) {
-                    $table[$i] = trim(ucfirst($table[$i]), "s"); // capitalizes first letter + remove s off the end
+                for ($i = 0;$i < $numOfFields; ++$i) {
+                    $table[$i] = trim(ucfirst($table[$i]),"s");
                     $tempResults[$table[$i]][$field[$i]] = $row[$i];
                 }
-
                 if ($singleResult == 1) {
                     mysql_free_result($this->_result);
                     return $tempResults;
                 }
-
-                array_push($result, $tempResults);
+                array_push($result,$tempResults);
             }
-
             mysql_free_result($this->_result);
             return($result);
         }
+        
+
     }
 
     /** Get number of rows **/
     function getNumRows() {
         return mysql_num_rows($this->_result);
     }
- 
+
     /** Free resources allocated by a query **/
- 
+
     function freeResult() {
         mysql_free_result($this->_result);
     }
- 
+
     /** Get error string **/
- 
+
     function getError() {
         return mysql_error($this->_dbHandle);
-    }    
+    }
 }
